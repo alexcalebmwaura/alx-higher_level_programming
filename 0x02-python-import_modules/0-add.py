b5c41d6234df47c77d3558285ba4ee1150412cf2
alexcalebmwaura <alexcalebmwaura@gamil.com>
2023-08-11 08:59:50 +0000

    Task done

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     """The program prints the sum of two numbers"""
     from add_0 import add
 
+
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
