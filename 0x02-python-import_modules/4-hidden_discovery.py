3a134552dd2ac29b3ad8ecb8c708fafb122cc352
alexcalebmwaura <alexcalebmwaura@gamil.com>
2023-08-11 09:35:59 +0000

    Task done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import hidden_4
 
     names = dir(hidden_4)
-    for name in names:
-        if names[:2] != "__":
+    for n in names:
+        if n[:2] != "__":
             print(name)
