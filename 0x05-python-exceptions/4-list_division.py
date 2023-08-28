eb0532f4da9d49d2769fee305d5fcaa3df43909a
alexcalebmwaura <alexcalebmwaura@gamil.com>
2023-08-28 20:14:23 +0000

    Task done

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-new_list = []
+
+    new_list = []
+
+
 for a in range(0, list_length):
     try:
         div = my_list_1[a] / my_list_2[a]
@@ -15,4 +18,4 @@ for a in range(0, list_length):
         div = 0
     finally:
         new_list.append(div)
-        return (new_list)
+    return (new_list)
