a958efb660486897267548e612843d9bc29803af
alexcalebmwaura <alexcalebmwaura@gamil.com>
2023-08-29 15:26:57 +0000

    Task done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """Define a square."""
+
+    def __init__(self, size):
+        """Initialize the new Square.
+
+        Args:
+            size (int): Size of new square.
+        """
+        self.__size = size
