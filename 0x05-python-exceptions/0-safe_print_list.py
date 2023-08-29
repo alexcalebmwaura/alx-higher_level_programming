38c4b73b2adfb35975aa7066c46d3bae9482215c
alexcalebmwaura <alexcalebmwaura@gamil.com>
2023-08-29 05:59:12 +0000

    Task done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
+
     ret = 0
-    for a in range(x):
+    for i in range(x):
         try:
-            print("{}".format(my_list[a]), end="")
+            print("{}".format(my_list[i]), end="")
             ret += 1
         except IndexError:
             break
